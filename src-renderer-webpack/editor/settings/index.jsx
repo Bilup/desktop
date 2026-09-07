ed19ad7487dbb80a6add4a70f80e642737e62f6d
DLGrass <2649771611@qq.com>
2026-09-07 20:42:59 +0800

    fix: try fix settings x3

--- src-renderer-webpack/editor/settings/index.jsx
+++ src-renderer-webpack/editor/settings/index.jsx
@@ -1,7 +1,6 @@
 import {addLocaleData, IntlProvider} from 'react-intl';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {MemoryRouter} from 'react-router-dom';
 
 import {localeData} from '@bilup/scratch-l10n';
 import editorMessages from '@bilup/scratch-l10n/locales/editor-msgs';
@@ -10,7 +9,7 @@ import communityTranslations from 'scratch-gui/src/community/translations/zh-cn.
 import IntlBridge from 'scratch-gui/src/lib/tw-use-intl.jsx';
 import {detectLocale} from 'scratch-gui/src/lib/utils/detect-locale.js';
 import {UserProvider} from 'scratch-gui/src/community/UserContext.jsx';
-import Settings from 'scratch-gui/src/community/pages/Settings.jsx';
+import Settings from './settings-page.jsx';
 import ErrorContainerHOC from '../error/error-container-hoc.jsx';
 
 const SafeSettings = ErrorContainerHOC(Settings);
@@ -37,19 +36,11 @@ document.body.classList.add('tw-loaded');
 
 ReactDOM.render(
   <IntlProvider locale={locale} messages={editorMessages[locale]}>
-    {/*
-      Settings 页面来自 gui 社区站,内部会使用 react-router 的
-      useSearchParams/useLocation(在网站里它运行在 Router 内)。桌面端是
-      独立渲染的裸页面,这里补一个 MemoryRouter,否则会触发
-      invariant 抛错导致整页白屏。
-    */}
-    <MemoryRouter initialEntries={['/settings']}>
-      <IntlBridge>
-        <UserProvider>
-          <SafeSettings isScratchDesktop={true} />
-        </UserProvider>
-      </IntlBridge>
-    </MemoryRouter>
+    <IntlBridge>
+      <UserProvider>
+        <SafeSettings isScratchDesktop={true} />
+      </UserProvider>
+    </IntlBridge>
   </IntlProvider>,
   appTarget
 );
